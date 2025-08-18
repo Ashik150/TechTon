681d6b6df9f20e9c182bd51d266a1a7dba39db65
Ashik150 <sskhan4409@gmail.com>
2025-08-18 17:16:24 +0600

    create event api created

---
+++ backend/controllers/event.controller.js
@@ -0,0 +1,49 @@
+import cloudinary from 'cloudinary';
+import { Shop } from '../models/shop.model.js';
+import ErrorHandler from '../Utils/ErrorHandler.js';
+import { Event } from '../models/event.model.js';
+
+export const createEvent = async (req, res, next) => {
+    try {
+        const shopId = req.body.shopId;
+        const shop = await Shop.findById(shopId);
+        if (!shop) {
+            return next(new ErrorHandler('Shop not found', 400));
+        } else {
+            let images = [];
+
+            if (typeof req.body.images === "string") {
+                images.push(req.body.images);
+            } else {
+                images = req.body.images;
+            }
+
+            const imagesLinks = [];
+
+            for (let i = 0; i < images.length; i++) {
+                const result = await cloudinary.uploader.upload(images[i], {
+                    folder: "products",
+                });
+
+                imagesLinks.push({
+                    public_id: result.public_id,
+                    url: result.secure_url,
+                });
+            }
+
+            const productData = req.body;
+            productData.images = imagesLinks;
+            productData.shop = shop;
+
+            const event = await Event.create(productData);
+
+            res.status(201).json({
+                success: true,
+                event,
+            });
+        }
+
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+};
